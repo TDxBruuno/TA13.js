0c4e757a2c31ee1e3e75148f833f5f250e0c4c0d
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-08-19 09:42:29 -0300

    listo

---
+++ app.js
@@ -0,0 +1,6 @@
+let boton = document.querySelector("button");
+let parrafo = document.querySelector("p");
+
+boton.addEventListener("click", function () {
+  parrafo.textContent = "Texto Cambiado";
+});
